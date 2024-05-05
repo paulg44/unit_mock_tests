5cc4fa92bc7aa5eb1c9fbefdd89049946f78888e
Paul Garton <paulgarton1984@hotmail.com>
2024-05-05 10:30:39 +0100

    add one more test, think I have now stopped deployment on github, so it shouldnt fail

--- client/tests/test.test.js
+++ client/tests/test.test.js
@@ -8,3 +8,7 @@ test("adds 1 + 2 to equal 3", () => {
 test("add 2 + 2 to equal 4", () => {
   expect(sum(2, 2)).toBe(4);
 });
+
+test("add 2 + 3 to equal 5", () => {
+  expect(sum(2, 3)).toBe(5);
+});
