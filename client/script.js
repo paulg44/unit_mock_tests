37811536207fac6f2a6ee5f6301432b62c1c1c0f
Paul Garton <paulgarton1984@hotmail.com>
2024-05-06 10:58:42 +0100

    add shop items to display

--- client/script.js
+++ client/script.js
@@ -3,17 +3,40 @@ const dev = "http://localhost:5030/api/shop";
 const deploy = "https://unit-mock-tests.onrender.com/api/shop";
 
 async function fetchAllShopItems() {
-  const shopItemsResponse = await fetch(`${dev}`).then((shopItemsResponse) => {
-    return shopItemsResponse.json();
-  });
-  console.log(shopItemsResponse);
-  const items = shopItemsResponse.map((item) => item.item);
-  const price = shopItemsResponse.map((price) => price.price);
+  try {
+    const shopItemsResponse = await fetch(`${dev}`);
+
+    const shopItemsData = await shopItemsResponse.json();
+
+    // Call function so I can use data outside of fetch
+    displayShopItems(shopItemsData);
+    console.log(shopItemsResponse);
+  } catch (error) {
+    console.error("Error fetching shop items", error);
+  }
+}
+
+function displayShopItems(data) {
+  console.log(data);
+  const items = data.map((item) => item.item);
+  const price = data.map((price) => price.price);
+  const description = data.map((description) => description.description);
   const shopContainer = document.querySelector(".shop-container");
-  shopContainer.innerHTML = `<div>
-  <p>${items}</p>
-  <p>£${price}</p>
-  <div>`;
+  shopContainer.innerHTML = items
+    .map(
+      (item, index) =>
+        `
+         <div class="item-title-image">
+            <p>${item}</p>
+          </div>
+          <div class="description-price">
+            <p>£${price[index]}</p>
+            <p>${description[index]}</p>
+          </div>
+          <button type="button">Add to cart</button>
+        `
+    )
+    .join("");
 }
 
 fetchAllShopItems();
