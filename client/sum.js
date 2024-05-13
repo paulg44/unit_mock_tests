6ed53217ca75f488c70b12883d474bccb00f2da2
Paul Garton <paulgarton1984@hotmail.com>
2024-05-13 21:19:32 +0100

    write test first then code after

--- client/sum.js
+++ client/sum.js
@@ -3,4 +3,13 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum;
+// let totalArr = [9.99, 12.99];
+
+function arrayTotal(array) {
+  return array.reduce((acc, curr) => acc + curr, 0);
+}
+
+// console.log(arrayTotal(totalArr));
+
+module.exports = { sum, arrayTotal };
+// module.exports.arrayTotal = arrayTotal;
--- client/tests/test.test.js
+++ client/tests/test.test.js
@@ -1,5 +1,5 @@
-// Test file for tests
-const sum = require("../sum");
+// Test file for writing tests before writing basic code functions
+const { sum, arrayTotal } = require("../sum");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -11,4 +11,11 @@ test("add 2 + 2 to equal 4", () => {
 
 test("add 2 + 3 to equal 5", () => {
   expect(sum(2, 3)).toBe(5);
+});
+
+// Test array total
+test("all elements in array correctly add up to total", () => {
+  let array = [9.99, 20];
+  // Need the too be close to because of the decimals in JS
+  expect(arrayTotal(array)).toBeCloseTo(29.99);
 });
