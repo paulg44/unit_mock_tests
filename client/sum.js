2bce2b013be72accf3250a922d838dca47ebbf37
Paul Garton <paulgarton1984@hotmail.com>
2024-05-14 19:58:34 +0100

    write test to add an element to array, then function after, passed!

--- client/sum.js
+++ client/sum.js
@@ -3,13 +3,21 @@ function sum(a, b) {
   return a + b;
 }
 
-// let totalArr = [9.99, 12.99];
+// Sum up all items in array
+let totalArr = [9.99, 12.99];
 
 function arrayTotal(array) {
   return array.reduce((acc, curr) => acc + curr, 0);
 }
+console.log(arrayTotal(totalArr));
 
-// console.log(arrayTotal(totalArr));
+// Add an item to array
+let addArr = ["trousers", "skirt"];
 
-module.exports = { sum, arrayTotal };
+function addToArray(array, item) {
+  return array.push(item);
+}
+console.log(addToArray(addArr, "shirt"), ...addArr);
+
+module.exports = { sum, arrayTotal, addToArray };
 // module.exports.arrayTotal = arrayTotal;
--- client/tests/test.test.js
+++ client/tests/test.test.js
@@ -1,5 +1,15 @@
 // Test file for writing tests before writing basic code functions
-const { sum, arrayTotal } = require("../sum");
+/* Test List 
+     - Jest basic tests
+     - All items in an array add up (shopping cart)
+     - Element can be removed from array 
+     - Element can be added to array (toContain)
+     - Add object to array
+     - Throw error?
+     - An input value can be changed
+     - 
+*/
+const { sum, arrayTotal, addToArray } = require("../sum");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -18,4 +28,12 @@ test("all elements in array correctly add up to total", () => {
   let array = [9.99, 20];
   // Need the too be close to because of the decimals in JS
   expect(arrayTotal(array)).toBeCloseTo(29.99);
+});
+
+// Add an element to array
+test("add element to array", async () => {
+  let arr = ["shorts", "jeans"];
+  let item = "shirt";
+  await addToArray(arr, item);
+  expect(arr).toContain(item);
 });
